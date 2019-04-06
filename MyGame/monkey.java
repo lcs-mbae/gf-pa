c25bbd0abd2869069305fc7bac7d671d91b52aa5
Minah Bae <MBae@lcs.on.ca>
2019-04-06 14:15:10 -0400

    moving monkey

--- MyGame/monkey.java
+++ MyGame/monkey.java
@@ -12,9 +12,30 @@ public class monkey extends Actor
      * Act - do whatever the monkey wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int reload = 60;
     public void act() 
     {
+        movement();
         
         
     }    
+    private void movement()
+    {
+     if (Greenfoot.isKeyDown("left"))
+     {
+         setLocation(getX()-3,getY());
+     }
+     if (Greenfoot.isKeyDown("right"))
+     {
+         setLocation(getX()+3,getY());
+     }
+     if (Greenfoot.isKeyDown("up"))
+     {
+         setLocation(getX(), getY()-3);
+     }
+     if (Greenfoot.isKeyDown("down"))
+     {
+         setLocation(getX(), getY()+3);
+     }
+    }
 }
